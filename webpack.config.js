13bb1a6aa1bb2aceddc7e4911bd3da26d0e528db
junpeko5 <junpeko5@gmail.com>
2019-02-10 22:20:16 +0900

    encoreのインストールとCSSのminファイル化

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// the project directory where all compiled assets will be stored
+  .setOutputPath('public/assets/')
+
+  // the public path used by the web server to access the previous directory
+  .setPublicPath('/assets')
+
+  // will create public/build/app.js and public/build/app.css
+  // .addEntry('app', './assets/js/app.js')
+  .addStyleEntry('css/dashboard', './assets/css/dashboard.css')
+  .addStyleEntry('css/login', './assets/css/login.css')
+
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
