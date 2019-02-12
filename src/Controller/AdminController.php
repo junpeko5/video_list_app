7b34191fef103da8473af82830574f72e4a3eed4
junpeko5 <junpeko5@gmail.com>
2019-02-12 23:37:13 +0900

    editページのカテゴリー表示

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -39,12 +39,14 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/edit_category", name="edit_category")
+     * @Route("/edit_category/{id}", name="edit_category")
      */
-    public function editCategory()
+    public function editCategory(Category $category)
     {
 
-        return $this->render('admin/edit_category.html.twig');
+        return $this->render('admin/edit_category.html.twig', [
+            'category' => $category
+        ]);
     }
 
 
@@ -86,12 +88,13 @@ class AdminController extends AbstractController
         return $this->render('admin/users.html.twig');
     }
 
-    public function getAllCategories(CategoryTreeAdminOptionList $categories)
+    public function getAllCategories(CategoryTreeAdminOptionList $categories, $editedCategory = null)
     {
+        dump($editedCategory);
         $categories->getCategoryList(($categories->buildTree()));
-        dump($categories);
         return $this->render('admin/_all_categories.html.twig', [
-            'categories' => $categories
+            'categories' => $categories,
+            'editedCategory' => $editedCategory,
         ]);
     }
 }
