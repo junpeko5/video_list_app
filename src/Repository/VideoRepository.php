712cd029cc380496ef2e507c7abb44d75f90ddbb
junpeko5 <junpeko5@gmail.com>
2019-02-19 00:06:31 +0900

    Sort機能の追加

--- src/Repository/VideoRepository.php
+++ src/Repository/VideoRepository.php
@@ -21,11 +21,19 @@ class VideoRepository extends ServiceEntityRepository
         $this->paginator = $paginator;
     }
 
-    public function findByChildIds(array $value, int $page)
+    /**
+     * @param array $value
+     * @param int $page
+     * @param string|null $sort_method
+     * @return \Knp\Component\Pager\Pagination\PaginationInterface
+     */
+    public function findByChildIds(array $value, int $page, ?string $sort_method)
     {
+        $sort_method = $sort_method != 'rating' ? $sort_method : 'ASC';
         $dbquery = $this->createQueryBuilder('v')
             ->andWhere('v.category IN (:val)')
             ->setParameter('val', $value)
+            ->orderBy('v.title', $sort_method)
             ->getQuery();
         $pagination = $this->paginator->paginate($dbquery, $page, 5);
         return $pagination;
