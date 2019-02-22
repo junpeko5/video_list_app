efa767a2cfee1cd69bfe5833c8a68abb7427c676
junpeko5 <junpeko5@gmail.com>
2019-02-23 05:23:01 +0900

    Comment Entityの作成

---
+++ src/Entity/Comment.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="comments")
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
+ */
+class Comment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    private $created_at2;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Video", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $video;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+
+    /**
+     * @ORM\PrePersist
+     * @return Comment
+     * @throws \Exception
+     */
+    public function setCreatedAt(): self
+    {
+        if(isset($this->created_at2))
+        {
+            $this->created_at = $this->created_at2;
+        }
+        else
+        {
+            $this->created_at = new \DateTime();
+        }
+        return $this;
+    }
+
+
+    /**
+     * @param $created_at
+     * @return Comment
+     */
+    public function setCreatedAtForFixtures($created_at): self
+    {
+        $this->created_at2 = $created_at;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getVideo(): ?Video
+    {
+        return $this->video;
+    }
+
+    public function setVideo(?Video $video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+}
