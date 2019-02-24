94f7bde07efae6db48bd42abd569860832089a07
junpeko5 <junpeko5@gmail.com>
2019-02-24 11:33:21 +0900

    like機能の作成、削除機能を実装

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -229,21 +229,41 @@ class FrontController extends AbstractController
 
     private function likeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->addLikedVideo($video);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
         return 'liked';
     }
 
     private function disLikeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->addDislikedVideo($video);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
         return 'disliked';
     }
 
     private function undoLikeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->removeLikedVideo($video);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
         return 'undo liked';
     }
 
     private function undoDislikeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->removeDislikedVideo($video);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
         return 'undo disliked';
     }
 
